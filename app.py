245d37e42d38ce3d69ab6f0cf2b81723491b605b
Anshika Arya <anshikaarya042@gmail.com>
2025-02-19 11:48:21 +0530

    dataingestion changes

--- app.py
+++ app.py
@@ -1,12 +1,11 @@
-from src.mlproject.logger import logging 
-from src.mlproject.exception import CustomException
-from src.mlproject.components.data_ingestion import DataIngestion
-from src.mlproject.components.data_ingestion import DataIngestionConfig
+from src.logger import logging 
+from src.exception import CustomException
+from src.components.data_ingestion import DataIngestion
+from src.components.data_ingestion import DataIngestionConfig
 import sys
 
 if __name__ == "__main__":
     logging.info("The execution has started")
-
 
     try:
         #data_ingestion_config = DataIngestionConfig()
--- setup.py
+++ setup.py
@@ -1,6 +1,8 @@
 from setuptools import find_packages,setup
 from typing import List
 
+Hypen_E_Dot='-e .'
+
 def get_requirements(file_path:str)->List[str]:
     ''''
     this function  will return the list of requirements
@@ -9,7 +11,10 @@ def get_requirements(file_path:str)->List[str]:
 
     with open(file_path) as file_obj:
         requirements=file_obj.readlines()
-        requirements=[req.replace('\n',"") for req in requirements ]
+        [req.replace('\n',"") for req in requirements ]
+        
+        if Hypen_E_Dot in requirements:
+            requirements.remove(Hypen_E_Dot)
     
     return requirements
 
